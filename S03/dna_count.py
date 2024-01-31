355ad019af6bb122f1de19ce851c69ed82d92bed
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-01-31 18:30:20 +0100

    new commit

--- S03/dna_count.py
+++ S03/dna_count.py
@@ -28,3 +28,23 @@ print("Number of times that letter 'A' appears in the sentence:",numberA)
 print("Number of times that letter 'C' appears in the sentence:",numberC)
 print("Number of times that letter 'G' appears in the sentence:",numberG)
 print("Number of times that letter 'T' appears in the sentence:",numberT)
+
+
+# With dicts
+sequence = input("Please enter a sequence: ")
+number_of_bases = len(sequence)
+print("Total length: " , number_of_bases)
+dic = {"A" : 0 , "C" : 0 , "G" : 0 , "T" : 0}
+for c in sequence:
+    if c == "A":
+        dic["A"] += 1
+    if c == "C":
+        dic["C"] += 1
+    if c == "G":
+        dic["G"] += 1
+    if c == "T":
+        dic["T"] += 1
+    else:
+       pass
+print(dic)
+
