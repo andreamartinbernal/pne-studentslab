7b876d4b45aee91ee31c97b7dc3e2ee1b755aa01
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-02-13 12:52:52 +0100

    p00 commits

--- P00/e6.py
+++ P00/e6.py
@@ -7,5 +7,5 @@ full_filename = Path(FOLDER) / FILENAME
 complete_seq = seq_read_fasta(full_filename)
 print("-----| Exercise 6 |------")
 print("Gene U5")
-print("Fragment:", complete_seq[0:20])
-print("Reverse :", seq_reverse(complete_seq, 20))
+print("Fragment:\t", complete_seq[0:20])
+print("Reverse:\t", seq_reverse(complete_seq, 20))
