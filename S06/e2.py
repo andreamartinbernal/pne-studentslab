7b4d7845d26c5edf82cef08a6f23b17c1d9ef510
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-02-19 12:11:31 +0100

    commits

--- S06/e2.py
+++ S06/e2.py
@@ -11,10 +11,11 @@ class Seq:
     def __len__(self):
         return len(self.strbases)
 
+
 def print_seqs(seq_list):
     n = 0
     for seq in seq_list:
-        print(f"Sequence {n} : (Length: {seq.len()}) {seq}")
+        print(f"Sequence {n} : (Length: {len(seq)}) {seq}")
         n += 1
 
 
