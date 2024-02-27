be6086257e10ac56a922b21780faacaba36b8bea
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-02-27 10:39:31 +0100

    commits

---
+++ P01/e6.py
@@ -0,0 +1,14 @@
+from Seq1 import *
+print("-----| Practice 1, Exercise 6 |------")
+seq1 = Seq()
+seq2 = Seq("TATAC")
+seq3 = Seq("TATAX")
+
+print(f'Sequence 1: (Length: {seq1.seq_len()}) {seq1.seq}')
+print('Bases:',  seq1.seq_count())
+
+print(f'Sequence 2: (Length: {seq2.seq_len()}) {seq2.seq}')
+print('Bases:',  seq2.seq_count())
+
+print(f'Sequence 3: (Length: {seq3.seq_len()}) {seq3.seq}')
+print('Bases:',  seq3.seq_count())
