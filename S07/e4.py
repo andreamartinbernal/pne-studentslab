8361dad382dde477dd8dc00443fcbe564407fbf8
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-02-28 16:33:33 +0100

    commits

---
+++ S07/e4.py
@@ -0,0 +1,1 @@
+#Con mi propia IP pueden hablar todos conmigo (distintos procesos distintos orenadores), pero con 127.0.0.1 solo puedo conectarme yo misma y nadie más(distintos procesos mismo ordenador)
