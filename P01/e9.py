7bdee54be5709cd59b5acd6f597e1fb32695a363
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-02-28 12:43:31 +0100

    commits

--- P01/e9.py
+++ P01/e9.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from Seq1 import *
 print("-----| Practice 1, Exercise 9 |------")
 seq = Seq()
-FOLDER = "./sequence/"
+FOLDER = "./../P00/sequences/"
 FILENAME = "U5.txt"
 full_filename = Path(FOLDER) / FILENAME
 seq.read_fasta(full_filename)
