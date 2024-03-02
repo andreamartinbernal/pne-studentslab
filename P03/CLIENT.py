acac75d9c1ef2d6fb19efd3449d0fbfafb77bff4
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-03-02 11:14:56 +0100

    commits

--- P03/CLIENT.py
+++ P03/CLIENT.py
@@ -3,7 +3,6 @@ from Client0 import Client
 PRACTICE = 2
 EXERCISE = 3
 
-print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
 IP = "127.0.0.1"  # your IP address
@@ -15,5 +14,5 @@ c = Client(IP, PORT)
 
 print(c)
 print("Sending a message to the server...")
-response = c.talk("GET 0")
+response = c.talk("GET 2")
 print(f"Response: {response}")
