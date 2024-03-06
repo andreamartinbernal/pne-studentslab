e5e5799e01d09a0e5adbe38c894768c37f0c9076
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-03-06 11:01:59 +0100

    commits

--- P03/PRACTICE_CLIENT.py
+++ P03/PRACTICE_CLIENT.py
@@ -16,7 +16,6 @@ print("\n* Testing PING...")
 response = c.talk("PING")
 print(response)
 print("\n* Testing GET...")
-
 for i in range(0, 5):
     response = c.talk(f"GET {i}")
     print(f"GET {i}: {response}")
@@ -34,20 +33,30 @@ for name in ["U5", "ADA", "FRAT1", "FXN", "RNU6_1155P"]:
     print(f"\nGENE {name}")
     response = c.talk(f"GENE {name}")
     print(response)
+
+
+
+
+print("\n* Testing LIST...")
+response = c.talk("LIST")
+for seq in SEQ_LIST:
+    print(seq)
+
 print("\n* Testing ADD...")
-seq1 = cs.decode(seq1)
+response = c.talk("LIST")
+seq1 = "AAAAGG"
 seq2 = "ACGTGGG"
-print("Adding", seq1, "and", seq2, "to the list")
+print(f"Adding {seq1} and {seq2} to the list")
 for sequence in SEQ_LIST:
     if seq1 == sequence and (len(seq1) == len(sequence)):
-        response = c.talk("The sequence:", seq1, "is already on the list")
+        response = c.talk(f"The sequence: {seq1} is already on the list")
         print(response)
-    if seq2 == sequence and (len(seq2) == len(sequence)):
-        response = c.talk("The sequence:", seq2, "is already on the list")
+    if seq1 == sequence and (len(seq1) == len(sequence)):
+        response = c.talk(f"The sequence: {seq1} is already on the list")
         print(response)
     else:
         SEQ_LIST = SEQ_LIST.append(seq1)
         SEQ_LIST = SEQ_LIST.append(seq2)
-        response = c.talk("The resultance sequence list is:", SEQ_LIST)
+        response = c.talk(f"The resultant sequence list is: {SEQ_LIST}")
 
 response = c.talk("")
