cad378b4b92c293ac900cef8f4f2701acbbf4956
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-04-23 16:17:18 +0200

    commits

--- S18/test-json-3.py
+++ S18/test-json-3.py
@@ -6,31 +6,28 @@ from pathlib import Path
 jsonstring = Path("people-3.json").read_text()
 
 # Create the object person from the json string
-person = json.loads(jsonstring)
+people = json.loads(jsonstring)
+for person in people:
+    print()
+    n = 0
+    termcolor.cprint("Name: ", 'green', end="")
+    print(person['Firstname', str(n+1)])
+    termcolor.cprint("Age: ", 'green', end="")
+    print(person['age', n+1])
 
-# Person is now a dictionary. We can read the values
-# associated to the fields 'Firstname', 'Lastname' and 'age'
+    # Get the phoneNumber list
+    phoneNumbers = person['phoneNumber', n+1]
 
-# Print the information on the console, in colors
-print()
-termcolor.cprint("Name: ", 'green', end="")
-print(person['Firstname'], person['Lastname'])
-termcolor.cprint("Age: ", 'green', end="")
-print(person['age'])
+    # Print the number of elements in the list
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
 
-# Get the phoneNumber list
-phoneNumbers = person['phoneNumber']
+    # Print all the numbers
+    for i, dictnum in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone " + str(i + 1) + ": ", 'blue')
 
-# Print the number of elements in the list
-termcolor.cprint("Phone numbers: ", 'green', end='')
-print(len(phoneNumbers))
-
-# Print all the numbers
-for i, dictnum in enumerate(phoneNumbers):
-    termcolor.cprint("  Phone " + str(i + 1) + ": ", 'blue')
-
-    # The element num contains 2 fields: number and type
-    termcolor.cprint("\t- Type: ", 'red', end='')
-    print(dictnum['type'])
-    termcolor.cprint("\t- Number: ", 'red', end='')
-    print(dictnum['number'])
+        # The element num contains 2 fields: number and type
+        termcolor.cprint("\t- Type: ", 'red', end='')
+        print(dictnum['type'])
+        termcolor.cprint("\t- Number: ", 'red', end='')
+        print(dictnum['number'])
